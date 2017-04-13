15810e9a90910139dc53e1078b4db8ec2dfcf5cd
jeff-bridgeman <jeff.bridgeman@outlook.com>
2017-04-12 21:34:34 -0500

    SDLKFJSDLKFJSDLKJLDkj
    SDLKFJSLDKFJDLSKFJLKdfjSDKFJLSKDFJLKSDf

--- learning/exercises/ProjectEuler/Maths/SUM.go
+++ learning/exercises/ProjectEuler/Maths/SUM.go
@@ -4,38 +4,30 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
-	"os"
+	"strings"
 )
 
 //Sumdigits HI
 func Sumdigits() int {
-	//var list []int = os.Open(sums.txt)
-	//var list []int
 
-	file, err := os.Open("M:/Jeff Bridgeman/Documents/GitHub/GoWorkspace/src/github.com/jeff.bridgeman/learning/exercises/ProjectEuler/Maths/sums.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
-
-	list := make([]byte, 6000)
-	count, err := file.Read(list)
+	file, err := ioutil.ReadFile("M:/Jeff Bridgeman/Documents/GitHub/GoWorkspace/src/github.com/jeff.bridgeman/learning/exercises/ProjectEuler/Maths/sums.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	testing, err := ioutil.ReadFile("M:/Jeff Bridgeman/Documents/GitHub/GoWorkspace/src/github.com/jeff.bridgeman/learning/exercises/ProjectEuler/Maths/sums.txt")
-	if err != nil {
-		log.Fatal(err)
+	lines := strings.Split(string(file), "\n")
+	var nums []string
+	nums = make([]string, len(lines))
+	for _, n := range lines { //Credit to Mustafa on Stack Overflow
+		//a, err := strconv.Atoi(n)
+		//fmt.Println(n)
+		nums = append(nums, n)
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 
-	//lines := strings.Split(string(testing), "\n")
-	//var log []int
-
-	fmt.Println("======================")
-	fmt.Printf("%q\n", list[:count])
-	fmt.Println("Length of list:", len(list))
-	fmt.Println("======================")
-	fmt.Println(string(testing))
-	return count
+	//fmt.Println("======================")
+	fmt.Println(lines)
+	return 42
 }
