a3c66bf242ff140e47e7d2eb0743220ebe5cdbbe
jeff-bridgeman <jeff.bridgeman@outlook.com>
2017-04-12 22:16:51 -0500

    POSSIBLE SOLUTION
    YAY COOKIES

--- learning/exercises/ProjectEuler/Maths/SUM.go
+++ learning/exercises/ProjectEuler/Maths/SUM.go
@@ -4,30 +4,37 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"strconv"
 	"strings"
 )
 
 //Sumdigits HI
-func Sumdigits() int {
+func Sumdigits() float64 {
 
 	file, err := ioutil.ReadFile("M:/Jeff Bridgeman/Documents/GitHub/GoWorkspace/src/github.com/jeff.bridgeman/learning/exercises/ProjectEuler/Maths/sums.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	lines := strings.Split(string(file), "\n")
-	var nums []string
-	nums = make([]string, len(lines))
+	lines := strings.Split(string(file), "\r\n")
+	var nums []float64
+	nums = make([]float64, len(lines))
+
+	fmt.Println("After creating nums: ", len(nums))
+
+	var sums float64
+	sums = 0
+
 	for _, n := range lines { //Credit to Mustafa on Stack Overflow
-		//a, err := strconv.Atoi(n)
 		//fmt.Println(n)
-		nums = append(nums, n)
+		a, err := strconv.ParseFloat(n, 64)
+		//fmt.Println(a)
+		sums = sums + a
+		nums = append(nums, a)
 		if err != nil {
 			log.Fatal(err)
 		}
 	}
 
-	//fmt.Println("======================")
-	fmt.Println(lines)
-	return 42
+	return sums
 }
--- learning/exercises/ProjectEuler/main/main.go
+++ learning/exercises/ProjectEuler/main/main.go
@@ -10,5 +10,6 @@ func main() {
 	//fmt.Println("These are the solutions for several of the Project Euler problems.")
 	//	fmt.Println("Fibonnaci Sequence up to 10 places --", maths.Fibonnaci(10))
 	//fmt.Println("Sum of Fibonnaci numbers up to 4 million -- ", maths.Evenfib(2))
-	fmt.Println(maths.Sumdigits())
+
+	fmt.Println("The sum is:", maths.Sumdigits())
 }
